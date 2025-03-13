36d3fbccc3b3f609862a0e1211166a85e1c3d27f
Carrillo-coder <A01567228@tec.mx>
2025-03-13 15:17:49 -0600

    V2 del gitignore

---
+++ Assets/Scripts/postinfo.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[System.Serializable]
+public class postinfo
+{
+    public int userId;
+    public int id;
+    public string title;
+    public string body;
+
+
+    // Given JSON input:
+    // {"name":"Dr Charles","lives":3,"health":0.8}
+    // this example will return a PlayerInfo object with
+    // name == "Dr Charles", lives == 3, and health == 0.8f.
+}
+
---
+++ Assets/Scripts/postsinfo.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class postsinfo
+{
+    public List<postinfo> posts;
+    //No funciona porque el arreglo del json no tiene nombre principal.
+
+}
